e8bafcefd142f09643c4ef29dcfcbbd8bdbdc582
elgar <juancarlosgarcia.an@gmail.com>
2026-02-05 10:48:22 +0100

    fin

--- app/src/main/java/net/iessochoa/sergiocontreras/rickandmortyapi/network/RickAndMortyDtos.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/rickandmortyapi/network/RickAndMortyDtos.kt
@@ -24,5 +24,66 @@ data class RickAndMortyCharacterDto(
     @SerialName(value="id") val id: Int,
     @SerialName(value="name") val name: String,
     @SerialName(value="status") val status: String,
-    @SerialName(value="species") val species: String
+    @SerialName(value="species") val species: String,
+    @SerialName("location") val location
+
 )
+
+/*
+* {
+    "name": "bulbasaur",
+    "height": 7,
+    "weight": 69,
+    "base_experience": 64,
+    "is_default": true
+}*/
+
+/*
+* @Serializable
+data class PokemonDetailDto(
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    @SerialName("base_experience") val experience: Int,
+    @SerialName("is_default") val isDefault: Boolean
+)*/
+
+/*
+* {
+  "tienda": {
+    "nombre": "TechZone",
+    "almacen": {
+      "seccion": "Periféricos",
+      "producto": {
+        "id": 101,
+        "nombre": "Ratón Gaming RGB",
+        "precio": 45.99
+      }
+    }
+  }
+}*/
+
+/*
+* @Serializable
+data class TiendaResponse(
+    val tienda: TiendaDto
+)
+
+@Serializable
+data class TiendaDto(
+    val nombre: String,
+    val almacen: AlmacenDto // Entramos a la segunda caja
+)
+
+@Serializable
+data class AlmacenDto(
+    val seccion: String,
+    val producto: ProductoDto // Entramos a la tercera caja
+)
+
+@Serializable
+data class ProductoDto(
+    val id: Int,
+    val nombre: String,
+    val precio: Double
+)*/
